153a096f7491251d434067fd8734ef2307352267
caiomcs04 <caio6c@yahoo.com.br>
2021-07-11 16:32:58 -0300

    1#commit(incremento do metodo switch para subistituir os ifs + pequena verificação de input válido para operação(de 1 a 4). caso incorreto loop para refazer operação com valor correto)

--- CursoLogicaCapitulo04/src/CalculadoraSimples.java
+++ CursoLogicaCapitulo04/src/CalculadoraSimples.java
@@ -3,31 +3,46 @@ import java.util.Scanner;
 public class CalculadoraSimples {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Menu da Calculadora - Escolha qual operação a fazer " + "\n" +
-                "Digite 1 para somar | 2  para subtrair | 3 para multiplicar | 4 para dividir  ");
-        Integer operacao = scanner.nextInt();
-
-        System.out.println("Esse programa irá fazer uma simples operação com dois numeros");
-        System.out.print("Digite o primeiro numero: ");
-        Double primeiroNumero = scanner.nextDouble();
-        System.out.print("Digite o segundo numero: ");
-        Double segundoNumero = scanner.nextDouble();
-
+        boolean check = true;
         Double resultado = null;
 
-        if (operacao.equals(1)){
-                resultado = primeiroNumero + segundoNumero;
-        }
-        if (operacao.equals(2)){
-                resultado = primeiroNumero - segundoNumero;
-        }
-        if (operacao.equals(3)){
-                resultado = primeiroNumero * segundoNumero;
-        }
-        if (operacao.equals(4)){
-                resultado = primeiroNumero / segundoNumero;
-        }
+        do {
+            check = true;
+            System.out.print("Menu da Calculadora - Escolha qual operação a fazer " + "\n" +
+                    "Digite 1 para somar | 2  para subtrair | 3 para multiplicar | 4 para dividir  ");
+            Integer operacao = scanner.nextInt();
+
+            System.out.println("Esse programa irá fazer uma simples operação com dois numeros");
+            System.out.print("Digite o primeiro numero: ");
+            Double primeiroNumero = scanner.nextDouble();
+            System.out.print("Digite o segundo numero: ");
+            Double segundoNumero = scanner.nextDouble();
+
+            switch (operacao) {
+                case (1): {
+                    resultado = primeiroNumero + segundoNumero;
+                    break;
+                }
+                case (2): {
+                    resultado = primeiroNumero - segundoNumero;
+                    break;
+                }
+                case (3): {
+                    resultado = primeiroNumero * segundoNumero;
+                    break;
+                }
+                case (4): {
+                    resultado = primeiroNumero / segundoNumero;
+                    break;
+                }
+                default: {
+                    System.out.println("\n####ERROR####\n" +
+                            "Operação escolhida inválida. Escolha novamente" +
+                            "\n####ERROR####\n");
+                    check = false;
+                }
+            }
+        }while(!check);
 
         System.out.println("Resultado: " + resultado);
 
